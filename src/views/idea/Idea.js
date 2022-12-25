40e3b8daf2792adad66cbee621730f80e51686bd
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2022-12-25 12:58:26 +0200

    add slider animation

--- src/views/idea/Idea.js
+++ src/views/idea/Idea.js
@@ -41,55 +41,23 @@ class Idea extends Component {
   };
 
   render() {
+    let sliderStyle = {
+      transform: `translateX(${this.state.activeIndex * -100}%)`,
+    };
+
     return (
       <div className="wrapper wrapper--idea">
         <h1 className="base-title">Idea title</h1>
 
         <div className="idea-section">
           <div className="idea-section__left">
-            <div className="idea-section__left-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              dolorum quis tempora, vel ex est delectus minus natus sint
-              eligendi id commodi minima soluta. Autem officiis quas ea tempore
-              ipsam, ducimus ratione voluptatem odit unde nisi suscipit est iste
-              animi voluptas excepturi quidem. Maiores recusandae veniam amet
-              delectus? Laudantium aperiam culpa accusantium veritatis aut! Ipsa
-              voluptatibus obcaecati, architecto sapiente voluptatum nulla
-              similique eius libero? Necessitatibus fuga quod quo atque ipsum
-              similique omnis doloribus nesciunt. Quam quas, officia, fuga error
-              obcaecati corporis saepe aliquam culpa facilis quidem qui fugit
-              quia. Maxime quisquam esse blanditiis aspernatur deleniti rem
-              aperiam. Ipsam, atque corrupti?Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quas dolorum quis tempora, vel ex
-              est delectus minus natus sint eligendi id commodi minima soluta.
-              Autem officiis quas ea tempore ipsam, ducimus ratione voluptatem
-              odit unde nisi suscipit est iste animi voluptas excepturi quidem.
-              Maiores recusandae veniam amet delectus? Laudantium aperiam culpa
-              accusantium veritatis aut! Ipsa voluptatibus obcaecati, architecto
-              sapiente voluptatum nulla similique eius libero? Necessitatibus
-              fuga quod quo atque ipsum similique omnis doloribus nesciunt. Quam
-              quas, officia, fuga error obcaecati corporis saepe aliquam culpa
-              facilis quidem qui fugit quia. Maxime quisquam esse blanditiis
-              aspernatur deleniti rem aperiam. Ipsam, atque corrupti?Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Quas dolorum quis
-              tempora, vel ex est delectus minus natus sint eligendi id commodi
-              minima soluta. Autem officiis quas ea tempore ipsam, ducimus
-              ratione voluptatem odit unde nisi suscipit est iste animi voluptas
-              excepturi quidem. Maiores recusandae veniam amet delectus?
-              Laudantium aperiam culpa accusantium veritatis aut! Ipsa
-              voluptatibus obcaecati, architecto sapiente voluptatum nulla
-              similique eius libero? Necessitatibus fuga quod quo atque ipsum
-              similique omnis doloribus nesciunt. Quam quas, officia, fuga error
-              obcaecati corporis saepe aliquam culpa facilis quidem qui fugit
-              quia. Maxime quisquam esse blanditiis aspernatur deleniti rem
-              aperiam. Ipsam, atque corrupti?
-            </div>
+            <div className="idea-section__left-text"></div>
 
             <div className="">btns here</div>
           </div>
 
           <div className="slider-container">
-            <div className="idea-slider slider-js">
+            <div className="idea-slider slider-js" style={sliderStyle}>
               {dataImgs.map(({ imgSrc, imgAlt }, i) => {
                 return (
                   <div
@@ -111,6 +79,11 @@ class Idea extends Component {
               >
                 prev
               </button>
+
+              <span className="slides-number">
+                {`${this.state.activeIndex + 1} / ${dataImgs.length}`}
+              </span>
+
               <button
                 className="slider-btn-js slider-btn-js-next"
                 type="button"
