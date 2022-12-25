6af6b5e941f679cdb751587c156a243a9ff06b9a
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2022-12-25 12:11:34 +0200

    made idea slider

--- src/views/gallery/Gallery.js
+++ src/views/gallery/Gallery.js
@@ -1,87 +1,101 @@
 import React from "react";
-import GalleryData from './galleryData';
-import GalleryModal from './GalleryModal';
-import './Gallery.scss';
+import GalleryData from "./galleryData";
+import GalleryModal from "./GalleryModal";
+import "./Gallery.scss";
 
 class Interests extends React.Component {
-	state = {
-		itemsToShow: 4,
-		activeSlide: null,
-		touchDistance: 0
-	}
+  state = {
+    itemsToShow: 4,
+    activeSlide: null,
+    touchDistance: 0,
+  };
 
-	openModal = (e, i) => {
-		this.setState ({
-			activeSlide: i
-		});
-	}
+  openModal = (e, i) => {
+    this.setState({
+      activeSlide: i,
+    });
+  };
 
-	closeModal = () => {
-		this.setState ({
-			activeSlide: null
-		});
-	}
+  closeModal = () => {
+    this.setState({
+      activeSlide: null,
+    });
+  };
 
-	next = () => {
-		const slide = this.state.activeSlide + 1 < GalleryData.slice(0, this.state.itemsToShow).length ? this.state.activeSlide + 1 : 0;
-		this.setState({
-			activeSlide: slide
-		});
-	}
+  next = () => {
+    const slide =
+      this.state.activeSlide + 1 <
+      GalleryData.slice(0, this.state.itemsToShow).length
+        ? this.state.activeSlide + 1
+        : 0;
+    this.setState({
+      activeSlide: slide,
+    });
+  };
 
-	prev = () => {
-		const slide = this.state.activeSlide - 1 < 0 ? GalleryData.slice(0, this.state.itemsToShow).length - 1 : this.state.activeSlide - 1;
-		this.setState({
-			activeSlide: slide
-		})
-	}
+  prev = () => {
+    const slide =
+      this.state.activeSlide - 1 < 0
+        ? GalleryData.slice(0, this.state.itemsToShow).length - 1
+        : this.state.activeSlide - 1;
+    this.setState({
+      activeSlide: slide,
+    });
+  };
 
-	showMore = () => {
-		if (this.state.itemsToShow < GalleryData.length) {
-			this.setState(state => ({
-				itemsToShow: state.itemsToShow + 4
-			}));
-		} else {
-			return GalleryData.length;
-		}
-	}
+  showMore = () => {
+    if (this.state.itemsToShow < GalleryData.length) {
+      this.setState((state) => ({
+        itemsToShow: state.itemsToShow + 4,
+      }));
+    } else {
+      return GalleryData.length;
+    }
+  };
 
-	render() {
-		const {itemsToShow, activeSlide} = this.state;
+  render() {
+    const { itemsToShow, activeSlide } = this.state;
 
-		return (
-			<div className="wrapper wrapper-container" >
-				<h1 className="base-title">Images</h1>
-				<h2 className="gallery-subtitle">For inspiration and just for fun</h2>
-				<div className="gallery-container">
-					<div className="gallery-grid">
-						{GalleryData.slice(0, itemsToShow).map(({img}, i) => {
-							return (
-								<div className="d-flex" onClick={(e) => this.openModal(e, i)} key={i}>
-									<img className="gallery-img" src={img} alt={'picture ' + i} />
-								</div>
-							)
-						})}
-					</div>
-					<button
-						className="a nav-link" 
-						style={{display: itemsToShow >= GalleryData.length ? 'none' : 'block'}}
-						onClick={this.showMore}>
-							See More
-					</button>
-					<GalleryModal 
-						modalData={GalleryData.slice(0, itemsToShow)}
-						activeSlide={activeSlide}
-						hasPrev={activeSlide > 0}
-						hasNext={activeSlide + 1 < GalleryData.slice(0, itemsToShow).length}
-						prev={this.prev}
-						next={this.next}
-						closeModal={this.closeModal}
-					/>
-				</div>
-			</div>
-		)
-	}
+    return (
+      <div className="wrapper wrapper-container">
+        <h1 className="base-title">Images</h1>
+        <h2 className="gallery-subtitle">For inspiration and just for fun</h2>
+        <div className="gallery-container">
+          <div className="gallery-grid">
+            {GalleryData.slice(0, itemsToShow).map(({ img }, i) => {
+              return (
+                <div
+                  className="d-flex"
+                  onClick={(e) => this.openModal(e, i)}
+                  key={i}
+                >
+                  <img className="gallery-img" src={img} alt={"picture " + i} />
+                </div>
+              );
+            })}
+          </div>
+          <button
+            className="a nav-link"
+            style={{
+              display: itemsToShow >= GalleryData.length ? "none" : "block",
+            }}
+            onClick={this.showMore}
+          >
+            See More
+          </button>
+          <GalleryModal
+            modalData={GalleryData.slice(0, itemsToShow)}
+            activeSlide={activeSlide}
+            hasPrev={activeSlide > 0}
+            hasNext={activeSlide + 1 < GalleryData.slice(0, itemsToShow).length}
+            prev={this.prev}
+            next={this.next}
+            closeModal={this.closeModal}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Interests;
--- src/views/idea/Idea.js
+++ src/views/idea/Idea.js
@@ -1,8 +1,45 @@
-import React from "react";
-
+import React, { Component } from "react";
 import "./Idea.scss";
 
-class Idea extends React.Component {
+import sliderImg1 from "./images/bm-l.jpg";
+
+const dataImgs = [
+  {
+    imgSrc: sliderImg1,
+    imgAlt: "idea picture",
+  },
+  {
+    imgSrc: sliderImg1,
+    imgAlt: "idea picture",
+  },
+  {
+    imgSrc: sliderImg1,
+    imgAlt: "idea picture",
+  },
+];
+
+class Idea extends Component {
+  // Simple react slider
+  state = {
+    activeIndex: 0,
+  };
+
+  clickNext = () => {
+    if (this.state.activeIndex < dataImgs.length - 1) {
+      this.setState({ activeIndex: this.state.activeIndex + 1 });
+    } else {
+      this.setState({ activeIndex: 0 });
+    }
+  };
+
+  clickPrev = () => {
+    if (this.state.activeIndex > 0) {
+      this.setState({ activeIndex: this.state.activeIndex - 1 });
+    } else {
+      this.setState({ activeIndex: dataImgs.length - 1 });
+    }
+  };
+
   render() {
     return (
       <div className="wrapper wrapper--idea">
@@ -50,7 +87,39 @@ class Idea extends React.Component {
 
             <div className="">btns here</div>
           </div>
-          <div className="idea-section__right">import slider here</div>
+
+          <div className="slider-container">
+            <div className="idea-slider slider-js">
+              {dataImgs.map(({ imgSrc, imgAlt }, i) => {
+                return (
+                  <div
+                    className="slider-item-js"
+                    data-active={i === this.state.activeIndex}
+                    key={i}
+                  >
+                    <img src={imgSrc} alt={imgAlt} />
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="slider-actions">
+              <button
+                className="slider-btn-js slider-btn-js-prev"
+                type="button"
+                onClick={this.clickPrev}
+              >
+                prev
+              </button>
+              <button
+                className="slider-btn-js slider-btn-js-next"
+                type="button"
+                onClick={this.clickNext}
+              >
+                next
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
