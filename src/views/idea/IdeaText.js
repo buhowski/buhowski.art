0fda708fbf27159f6d92e2bcb00db88f360efc6e
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2022-12-27 12:35:19 +0200

    tabs

--- src/views/idea/IdeaText.js
+++ src/views/idea/IdeaText.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ideaData from "./ideaData";
+import IdeaRu from "./IdeaRu";
 
 export default function IdeaText() {
   const [currentTab, setCurrentTab] = useState("1");
@@ -7,17 +7,17 @@ export default function IdeaText() {
   const tabs = [
     {
       id: 1,
-      tabTitle: "Tab 1",
-      content: "1111",
+      title: "ru",
+      content: <IdeaRu />,
     },
     {
       id: 2,
-      tabTitle: "Tab 2",
+      title: "en",
       content: "2222",
     },
     {
       id: 3,
-      tabTitle: "Tab 3",
+      title: "ua",
       content: "3333",
     },
   ];
@@ -28,6 +28,13 @@ export default function IdeaText() {
 
   return (
     <div className="idea-info">
+      {/* Copyright */}
+      <div className="idea-copy">
+        <p>
+          {new Date().getFullYear()} <span>&copy;</span> A. Tsiomakh
+        </p>
+      </div>
+
       <div className="idea-tabs">
         {tabs.map((tab, i) => (
           <button
@@ -38,100 +45,20 @@ export default function IdeaText() {
             onClick={handleTabClick}
             data-active={currentTab === `${tab.id}`}
           >
-            ru
+            {tab.title}
           </button>
         ))}
       </div>
 
-      <div className="content">
-        {tabs.map((tab, i) => (
-          <div key={i}>{currentTab === `${tab.id}` && tab.content}</div>
-        ))}
-      </div>
-
-      {ideaData.map((data, i) => {
-        return (
-          <div className="idea-text" key={i}>
-            <div className="idea-text__lang">
-              <div className="idea-block">
-                <h2 className="idea-block__title h2">{data.basicRu}</h2>
-                <p className="idea-block__text">{data.appSampleRu}</p>
-                <h3 className="h3">{data.possibilitiesTitleRu}</h3>
-                <ul>
-                  {data.possibilitiesStarRu.map((item, a) => {
-                    return (
-                      <li key={a}>
-                        {item}
-                        <span className="idea-block__star"> *</span>
-                      </li>
-                    );
-                  })}
-                  <li>{data.possibilitiesRu}</li>
-                </ul>
-                <p className="idea-block__desc">
-                  <span className="idea-block__star">* </span> &ndash;{" "}
-                  {data.branchDescRu}
-                </p>
-                <p className="idea-block__info">{data.designRu}</p>
-              </div>
-
-              <div className="idea-block">
-                <h2 className="h2">{data.conceptTitleRu}</h2>
-                <div className="idea-concept">
-                  <h3 className="h3">{data.journalistRu}</h3>
-                  {data.journalistInfoRu.map((journalist, j) => {
-                    return (
-                      <p key={j} className="idea-block__text">
-                        {journalist}
-                      </p>
-                    );
-                  })}
-                </div>
-                <div className="idea-concept">
-                  <h3 className="h3">{data.filmTitleRu}</h3>
-                  {data.filmInfoRu.map((filmInfo, f) => {
-                    return (
-                      <p key={f} className="idea-block__text">
-                        {filmInfo}
-                      </p>
-                    );
-                  })}
-                </div>
-              </div>
-
-              <div className="idea-block">
-                <h2 className="h2">{data.excursusTitleRu}</h2>
-                <p className="idea-block__text">{data.excursusInfoRu}</p>
-              </div>
-
-              <div className="idea-block">
-                <h2 className="h2">{data.whyTitleRu}</h2>
-
-                {data.whyInfoRu.map((why, b) => {
-                  return (
-                    <p key={b} className="idea-block__text">
-                      {why}
-                    </p>
-                  );
-                })}
-              </div>
-
-              <div className="idea-block idea-block--features">
-                <h2 className="h2">{data.featuresTitleRu}</h2>
-                <ul>
-                  {data.featuresItemRu.map((feature, c) => {
-                    return (
-                      <li key={c}>
-                        <span>&plusmn;</span> {feature}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+      {tabs.map((tab, i) => (
+        <div
+          className="idea-overflow"
+          data-content={currentTab === `${tab.id}`}
+          key={i}
+        >
+          {currentTab === `${tab.id}` && tab.content}
+        </div>
+      ))}
     </div>
   );
 }
