18828103775a2f630c37d5a46eea28c77c91d041
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2022-12-28 12:28:41 +0200

    finish eng

--- src/views/idea/access-to-idea/textData.js
+++ src/views/idea/access-to-idea/textData.js
@@ -5,7 +5,7 @@ const textData = [
     basicUa: ``,
 
     appSampleRu: `Приложение примера укомплектованного многофункционального журнала с возможностью ведения и развития любой производственной деятельности или же служащее для поиска информации, развлечения и беззаботного времяпрепровождения.`,
-    appSampleEn: ``,
+    appSampleEn: `The application is an example of a complete multifunctional magazine with the ability to maintain and develop any production activity or to search for information, entertainment and carefree pastime.`,
     appSampleUa: ``,
 
     possibilitiesTitleRu: `Возможности:`,
@@ -18,15 +18,21 @@ const textData = [
       `Видеостриминг`,
     ],
     possibilitiesRu: `Имиджборд и анонимный веб-форум`,
-    possibilitiesStarEn: [``, ``, ``],
+    possibilitiesStarEn: [
+      `Social network, photo/video/text microblogging`,
+      `Online cinema / video hosting`,
+      `Live streaming`,
+    ],
+    possibilitiesEn: `Imageboard and anonymous web forum`,
     possibilitiesStarUa: [``, ``, ``],
+    possibilitiesUa: ``,
 
     branchDescRu: `ветвь имеет свою производственную деятельность: Журналистика. Кино/мульт производство. Создание компьютерных игр.`,
-    branchDescEn: ``,
+    branchDescEn: `the branch has its own production activity: Journalism. Cinema production. Video game development.`,
     branchDescUa: ``,
 
     designRu: `Для представления примера главной страницы входа в приложение служат яркие/провокационные/сильные обложки журналов: Time, The New Yorker, National Geographic, etc. Будут создаваться и меняться в зависимости от текущего инфоповода для массового обсуждения.`,
-    designEn: ``,
+    designEn: `To represent an example of the main page of the application, use bright/provocative/strong magazine covers like Time, The New Yorker, National Geographic, etc. Will be created and changed depending on the current newsbreak for public discussion.`,
     designUa: ``,
 
     conceptTitleRu: `Main Concepts. Идеи к рубрикам:`,
@@ -42,7 +48,11 @@ const textData = [
       `Рассмотреть обзоры на: реклама, политика, бюрократия, вопрос цензуры, насилие, воспитание поколений, проблемы глобализма, развитие, перенаселение планеты, религия, эффект массы, состояние общества, etc.`,
       `Гонзо журналистика (разобрать суть, реинкарнировать). Ведение повествования от первого лица (группы лиц) - текст/видео-дневников. Нырять в самое пекло идиотизма проблемы изучая её изнутри или создавать определенные условия для её появления (смесь психологических экспериментов поведения, внедрения в секты, церемония аяуаски, посещение burning man, etc).`,
     ],
-    journalistInfoEn: [``, ``, ``],
+    journalistInfoEn: [
+      `A new narrative of modern breathing, diverse opinions, truth and dirty realism. A peculiar presentation of information without censorship, with the nature of the writing style of Hunter Thompson, Bukowski, Palahniuk, etc.`,
+      `Consider reviews on: advertising, politics, bureaucracy, the issue of censorship, violence, upbringing generations, globalization problems, overpopulation, religion, etc.`,
+      `Gonzo journalism (rethink the essence, reincarnate). Conducting narration in the first person or groups of people - text / video diaries. Dive deep into idiocy problems by studying it from the inside or creating certain conditions for its appearance (a mix of psychological behavioral experiments, infiltration into sects, an ayahuasca ceremony, the burning man, etc.).`,
+    ],
     journalistInfoUa: [``, ``, ``],
 
     filmTitleRu: `Игры/кино/мульт производство:`,
@@ -54,7 +64,11 @@ const textData = [
       `Идея игры: Предлагаю вашему вниманию нелинейную ролевую игру с открытым миром – Казаки РПГ. Простыми словами нужно всего лишь сделать то, что поляки сделали с Ведьмаком (в частности третья часть серии игр). Реализовать казаков для популяризации нового видения, тех казаков что запорожская сечь, дерзких, свободолюбивых, алкоголь употребляющих, из за чего и бесстрашных, разбойников и беспредельщиков, танцующих гопачок (гопак) как боевое искусство, ездящих на лошадях и ловких паркуристов. Реализовать красочные виды, архитектуру Украины тех времен и образ настоящего казака. Совместить реальные факты и события с мистикой и юмором произведений Гоголя или других источников с подобной мистикой схожей эпохи.`,
       `Дополнительно: студию правильного перевода (рус, укр, eng, “смешной перевод” - гоблин).`,
     ],
-    filmInfoEn: ``,
+    filmInfoEn: [
+      `Raising the film industry and gamedev from its knees. Start working on a lot of your own ideas/scenarios/shortcomings. Bring together cool, infinitely understanding, critically thinking specialists and start creating a new history of culture, art and technological development in the country.`,
+      `Game idea: I bring to your attention a non-linear role-playing game with an open world - Cossacks RPG. In simple words, we just need to do what the Poles did with The Witcher (in particular, the third part of the game series). Implement Cossacks to popularize a new vision. Those Cossacks that are Zaporizhzhya Sich, daring, freedom-loving people dancing hopak as a martial art, riding horses and dexterous parkourists. Realize colorful views, the architecture of Ukraine of those times and character of a real Cossack. Combine real facts and events with the mysticism and humor of Gogol's works or other sources with similar mysticism of a similar era.`,
+      `Additionally: studio of exact translation (rus, ukr, eng, “funny translation” - goblin).`,
+    ],
     filmInfoUa: ``,
 
     excursusTitleRu: `Excursus. Начальное производство.`,
@@ -62,7 +76,7 @@ const textData = [
     excursusTitleUa: ``,
 
     excursusInfoRu: `Первичный запуск задействует рубрику Журналистика и рабочий имиджборд с идеями, рекламой и продвижением остального, что должно способствовать начальному толчку, набору аудитории и дальнейшему развитию.`,
-    excursusInfoEn: ``,
+    excursusInfoEn: `The first launch will use the Journalism scenario and working Imageboard with feature ideas and promotion of the rest, which should help for initial push, capture audience and further development.`,
     excursusInfoUa: ``,
 
     whyTitleRu: `Почему(?):`,
@@ -72,9 +86,13 @@ const textData = [
     whyInfoRu: [
       `Все текущие социальные сети превратились или превращаются в “телевизоры былой эпохи”, где вокруг сплошная цензура и поток нескончаемой рекламы. Выбор просматриваемого контента всё реже остается за пользователем. Поэтому и нужна новая свободная площадка, свежий воздух.`,
       `Для Украины это будет первое, своего рода, место для раскрытия творческого потенциала, создании и реализации личности, открытости и совершенно нового виденья. В этом приложении и будут собраны лучшие возможности воедино, без лишней мишуры и ненужных функций, с умопомрачительным минималистически-модернистским дизайном, футуристическим ответвлением и максимальной отзывчивостью к потребителю.`,
-      `На фоне текущей войны в стране и после очевидной победы Украины такое место идеально займет свою нишу производства всего выше перечисленного.`,
+      `На фоне текущей войны в стране и после очевидной победы Украины такое место идеально займет свою нишу производства всего вышеперечисленного.`,
     ],
-    whyInfoEn: [``, ``, ``],
+    whyInfoEn: [
+      `All current social networks have turned into “bygone era TVs”. Everywhere continuous censorship and a stream of endless advertising. The choice of viewed content is less often to be left for the user. Therefore, we need a new free platform with fresh air.`,
+      `For Ukraine it will be the first kind of place for the disclosure of creative potential, the creation and realization of personality, openness and a completely new vision. This application will bring the best features together, without unnecessary functions and useless things, with a breathtaking modern minimalist design, futuristic branch and maximum responsiveness to the consumer.`,
+      `Against the backdrop of the current war in the country and after the obvious victory of Ukraine, such a place will ideally occupy its niche for the production of all above listed.`,
+    ],
     whyInfoUa: [``, ``, ``],
 
     // Features ideas - not needed for now
