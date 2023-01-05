e5bc368044fb5f6b7933f50c3075194e0693e10e
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2023-01-05 23:55:14 +0200

    replace link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,92 +1,93 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import MediaQuery from "react-responsive";
-import Socials from "../socials/Socials";
-import "./Header.scss";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
+import Socials from '../socials/Socials';
+import './Header.scss';
 
 const linksData = [
-  {
-    pageLink: "about",
-    pageName: "About Me",
-  },
-  {
-    pageLink: "projects",
-    pageName: "Projects",
-  },
-  // {
-  //   pageLink: "gallery",
-  //   pageName: "Gallery",
-  // },
-  {
-    pageLink: "/idea",
-    pageName: "My Idea",
-  },
-  // {
-  //   pageLink: "/access-to-idea",
-  //   pageName: "access to idea",
-  // },
+	{
+		pageLink: 'idea',
+		pageName: 'My Idea',
+	},
+	{
+		pageLink: 'about',
+		pageName: 'About Me',
+	},
+	{
+		pageLink: 'projects',
+		pageName: 'Projects',
+	},
+	// {
+	//   pageLink: "gallery",
+	//   pageName: "Gallery",
+	// },
+
+	// {
+	//   pageLink: "/access-to-idea",
+	//   pageName: "access to idea",
+	// },
 ];
 const Header = () => {
-  const [menuOpen, setMenuOpen] = React.useState(false);
-  const navLinkItems = linksData.map(({ pageLink, pageName }, i) => {
-    return (
-      <li className="nav-item d-flex" key={i}>
-        <NavLink className="a nav-link" to={`${pageLink}`} end>
-          {pageName}
-        </NavLink>
-      </li>
-    );
-  });
-  const LogoNavLink = () => {
-    return (
-      <div className="logo d-flex">
-        <NavLink className="a logo-link" to="/" end>
-          <span className="tag-color">&lt;</span>
-          <span>Buhowski</span>
-          <span className="tag-color">/&gt;</span>
-        </NavLink>
-      </div>
-    );
-  };
+	const [menuOpen, setMenuOpen] = React.useState(false);
+	const navLinkItems = linksData.map(({ pageLink, pageName }, i) => {
+		return (
+			<li className="nav-item d-flex" key={i}>
+				<NavLink className="a nav-link" to={`${pageLink}`} end>
+					{pageName}
+				</NavLink>
+			</li>
+		);
+	});
+	const LogoNavLink = () => {
+		return (
+			<div className="logo d-flex">
+				<NavLink className="a logo-link" to="/" end>
+					<span className="tag-color">&lt;</span>
+					<span>Buhowski</span>
+					<span className="tag-color">/&gt;</span>
+				</NavLink>
+			</div>
+		);
+	};
 
-  return (
-    <header className={`header d-flex-c${menuOpen ? " header-overflow" : ""}`}>
-      <div className="wrapper">
-        <MediaQuery minWidth={769}>
-          <div className="d-flex-c-b">
-            <LogoNavLink />
-            <nav>
-              <ul className="nav d-flex-c">{navLinkItems}</ul>
-            </nav>
-          </div>
-        </MediaQuery>
-        <MediaQuery maxWidth={768}>
-          <LogoNavLink />
-          <div
-            className="mobile-menu-btn"
-            onClick={() => {
-              setMenuOpen((o) => !o);
-            }}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <nav className={`pos-abs mobile-menu${menuOpen ? " open" : ""}`}>
-            <div className="d-flex-c-c">
-              <div>
-                <ul className="nav">{navLinkItems}</ul>
-                <Socials />
-              </div>
-            </div>
-          </nav>
-        </MediaQuery>
-      </div>
-    </header>
-  );
+	return (
+		<header className={`header d-flex-c${menuOpen ? ' header-overflow' : ''}`}>
+			<div className="wrapper">
+				<MediaQuery minWidth={769}>
+					<div className="d-flex-c-b">
+						<LogoNavLink />
+						<nav>
+							<ul className="nav d-flex-c">{navLinkItems}</ul>
+						</nav>
+					</div>
+				</MediaQuery>
+				<MediaQuery maxWidth={768}>
+					<LogoNavLink />
+					<div
+						className="mobile-menu-btn"
+						onClick={() => {
+							setMenuOpen((o) => !o);
+						}}
+					>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+					</div>
+					<nav className={`pos-abs mobile-menu${menuOpen ? ' open' : ''}`}>
+						<div className="d-flex-c-c">
+							<div>
+								<ul className="nav">{navLinkItems}</ul>
+								<Socials />
+							</div>
+						</div>
+					</nav>
+				</MediaQuery>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
