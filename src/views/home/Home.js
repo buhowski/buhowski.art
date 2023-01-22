047703aa3b203ca9953b77b5f2a0dda029a9a213
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2023-01-22 10:48:20 +0200

    add more space

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,25 +1,25 @@
-import React from "react";
-import useScripts from "../../components/use-scripts";
-import Socials from "../../components/socials/Socials";
+import React from 'react';
+import useScripts from '../../components/use-scripts';
+import Socials from '../../components/socials/Socials';
 
-import "./Home.scss";
+import './Home.scss';
 
 const Home = () => {
-  useScripts("js/chunk-app-drawing.js");
-  useScripts("js/app-drawing.js");
+	useScripts('js/chunk-app-drawing.js');
+	useScripts('js/app-drawing.js');
 
-  return (
-    <div className="wrapper home-wrapper d-flex">
-      <div className="name-container">
-        <h1 className="base-title">Alexander Tsiomakh</h1>
-        <h2 className="tag-color">Front-End Developer</h2>
-        <Socials />
-      </div>
+	return (
+		<div className='wrapper home-wrapper d-flex'>
+			<div className='name-container'>
+				<h1 className='base-title'>Alexander Tsiomakh</h1>
+				<h2 className='tag-color'>Front-End Developer. Writer</h2>
+				<Socials />
+			</div>
 
-      {/*############## CONTENT WITH PHOTO DRAWING ##############*/}
-      <div id="app"></div>
-    </div>
-  );
+			{/*############## CONTENT WITH PHOTO DRAWING ##############*/}
+			<div id='app'></div>
+		</div>
+	);
 };
 
 export default Home;
