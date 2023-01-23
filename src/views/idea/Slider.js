9a87f58059fd8bfe5a95f5b1da4affb4f0c17f2b
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2023-01-23 18:51:22 +0200

    change slider add artworks

--- src/views/idea/Slider.js
+++ src/views/idea/Slider.js
@@ -1,31 +1,66 @@
 import React, { Component } from 'react';
-import sliderImg1 from './images/bm.jpg';
+import sliderImg1 from './images/ai.jpg';
 import sliderImg2 from './images/einstein.jpg';
-import sliderImg3 from './images/mona-cat.png';
-import sliderImg4 from './images/times.jpg';
-import sliderImg5 from './images/hyperbolic.jpg';
+import sliderImg3 from './images/architect.jpg';
+import sliderImg4 from './images/evolution.jpg';
+import sliderImg5 from './images/mona-cat.png';
+import sliderImg6 from './images/ow.jpg';
+import sliderImg7 from './images/paint.jpg';
+import sliderImg8 from './images/paper.jpg';
+import sliderImg9 from './images/startup.jpg';
+import sliderImg10 from './images/times.jpg';
+import sliderImg11 from './images/write.jpg';
+import sliderImg12 from './images/chinking.jpg';
 import PopupContacts from './PopupContacts';
 
 const dataImgs = [
 	{
+		imgSrc: sliderImg1,
+		imgAlt: 'ai picture',
+	},
+	{
 		imgSrc: sliderImg2,
-		imgAlt: 'picture just for design',
+		imgAlt: 'einstein picture',
 	},
 	{
 		imgSrc: sliderImg3,
-		imgAlt: 'picture just for design',
-	},
-	{
-		imgSrc: sliderImg1,
-		imgAlt: 'picture just for design',
+		imgAlt: 'architect picture',
 	},
 	{
 		imgSrc: sliderImg4,
-		imgAlt: 'picture just for design',
+		imgAlt: 'evolution picture',
 	},
 	{
 		imgSrc: sliderImg5,
-		imgAlt: 'picture just for design',
+		imgAlt: 'mona-cat picture',
+	},
+	{
+		imgSrc: sliderImg6,
+		imgAlt: 'ow picture',
+	},
+	{
+		imgSrc: sliderImg7,
+		imgAlt: 'paint picture',
+	},
+	{
+		imgSrc: sliderImg8,
+		imgAlt: 'paper picture',
+	},
+	{
+		imgSrc: sliderImg9,
+		imgAlt: 'startup picture',
+	},
+	{
+		imgSrc: sliderImg10,
+		imgAlt: 'times picture',
+	},
+	{
+		imgSrc: sliderImg11,
+		imgAlt: 'write picture',
+	},
+	{
+		imgSrc: sliderImg12,
+		imgAlt: 'chinking picture',
 	},
 ];
 
@@ -53,14 +88,11 @@ class SliderContainer extends Component {
 
 	render() {
 		const { contactBtnTitle } = this.props;
-		let sliderStyle = {
-			transform: `translateX(${this.state.activeIndex * -100}%)`,
-		};
 
 		return (
 			<div className='slider-with-btn'>
 				<div className='slider-container'>
-					<div className='idea-slider slider-js' style={sliderStyle}>
+					<div className='idea-slider slider-js'>
 						{dataImgs.map(({ imgSrc, imgAlt }, i) => (
 							<div
 								className='slider-item-js'
