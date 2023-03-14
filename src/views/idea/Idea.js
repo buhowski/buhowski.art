cb0dfea93465bc8fab41a06fb13b3b0374ea5afa
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2023-03-14 08:35:08 +0200

    text

--- src/views/idea/Idea.js
+++ src/views/idea/Idea.js
@@ -6,8 +6,7 @@ import textData from './textData';
 export default function Idea() {
 	const contactBtnTitle = 'Get In Touch';
 	const baseTitle = 'Pure Details';
-	const baseDesc =
-		'Space of endless possibilities, an offer to invest in huge potential!';
+	const baseDesc = 'An offer to invest in huge potential!';
 
 	const textItems = textData.map((data, i) => (
 		<div key={i}>
