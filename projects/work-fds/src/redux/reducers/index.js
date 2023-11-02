f9c182c713e97c5ac472f8e5f71a7d57667f903b
Alexander T <a.tsiomakh@gmail.com>
2023-11-02 12:52:04 +0200

    ok

--- projects/work-fds/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import servicesReducer from './servicesReducer';
-
-const rootReducer = combineReducers({
-  servicesReducer,
-  // add more reducers here
-});
-
-export default rootReducer;
